c20b02602319a1de6a04284cd6abdf909587efb2
Tomas Diaz <tomas_diaz@live.cl>
2022-05-09 20:44:30 -0400

    Creaccion de tabla y reestructuración de codigo

--- js/main.js
+++ js/main.js
@@ -1,66 +1,89 @@
+const llenarTabla = document.querySelector('#lista-tabla tbody');
+const notificacion = document.getElementById('alert-error');
+
+
+
 function calcular(){
-   //let nombre = document.getElementById('nombre').value;
-   //let email = document.getElementById('inputEmail4').value;
-   //let celular = document.getElementById('celular').value;
-   let monto = document.getElementById('monto').value;
-   let cuotas = document.getElementById('cuotas').value;
-   let interes = document.getElementById('interes').value;
-   let renta = document.getElementById('renta').value;
-   const calcularCredito = calcularInteres(monto, interes, cuotas);
-   console.table(calcularCredito)
-   optionList(renta, calcularCredito); 
+//let nombre = document.getElementById('nombre').value;
+//let email = document.getElementById('inputEmail4').value;
+//let celular = document.getElementById('celular').value;
+let monto = document.getElementById('monto').value;
+let interes = document.getElementById('interes').value;
+let renta = document.getElementById('renta').value;
+let cuotas =  document.getElementById('cuotas').value;
+
+console.log('cuotas',cuotas)
+
+const calcularCredito = calcularInteres(monto, interes, cuotas);
+showInformationCredit(cuotas,calcularCredito,renta); 
 }
 function calcularInteres(monto, interes, cuotas){
-    const meses = [
-        "Enero",
-        "Febrero",
-        "Marzo",
-        "Abril",
-        "Mayo",
-        "Junio",
-        "Julio",
-        "Agosto",
-        "Septiembre",
-        "Octubre",
-        "Noviembre",
-        "Diciembre",
-    ]; 
-    let valorCuotas = (parseInt(monto) *  parseFloat(interes)) / parseInt(cuotas); 
-    let totalCredito = (parseInt(monto) *  parseFloat(interes)); 
-    let credito = {
-        'valorCuota': valorCuotas,
-        'totalCredito': totalCredito
-    };
-    for(i =0; i<parseInt(cuotas);i++){
-        document.write(`mes: ${meses[i]}\n valor: ${Math.round(valorCuotas)}<br>`)
-        }
-   /*  let findMes = meses.find((x)=> x=='Marzo');
-    console.log('posicion: ', findMes) */
-    return credito;
+let valorCuotas = (parseInt(monto) *  parseFloat(interes)) / parseInt(cuotas); 
+let totalCredito = (parseInt(monto) *  parseFloat(interes)); 
+let credito = {
+    'valorCuota': valorCuotas,
+    'totalCredito': totalCredito
+};
+return credito;
 }
 
-function evaluacionComercial(renta, montoCredito){
-    const rentaMinima = parseInt(montoCredito) * 0.5;
-    if(parseInt(renta) > parseInt(rentaMinima)){
-        alert('Usted puede optar al credito')
+function evaluacionComercial(montoCredito,renta){
+let evaluacionComercial = {
+    opta: false,
+    mensaje: ''
+};
+const rentaMinima = parseInt(montoCredito) * 0.5;
+if(parseInt(renta) > parseInt(rentaMinima)){
+    evaluacionComercial.opta = true;
+    evaluacionComercial.mensaje = 'Usted puede optar al credito';
+    return evaluacionComercial;
+}else{
+    evaluacionComercial.opta = false;
+    evaluacionComercial.mensaje = 'Usted no puede optar al credito';
+    return evaluacionComercial;
+}
+
+}
+
+const showInformationCredit = (cuotas,calcularCredito,renta) => {
+
+  const meses = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+]; 
+while(llenarTabla.firstChild) {
+    llenarTabla.removeChild(llenarTabla.firstChild);
+}
+
+    let evComercial = evaluacionComercial(calcularCredito['totalCredito'],renta);
+    notificacion.classList.remove('alert-success', 'alert-danger')
+    if(evComercial.opta){
+        notificacion.re
+        notificacion.className += " alert-success";
+        notificacion.hidden = false;
+        notificacion.innerHTML = evComercial.mensaje;
+        for (i =0; i<parseInt(cuotas);i++) {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${meses[i]}</td>
+                <td>${Math.round(calcularCredito['valorCuota'])}</td>
+            `;
+            llenarTabla.appendChild(row);
+        }
     }else{
-        alert('Usted no puede optar al credito') 
-    }
-    
-}
-
-const optionList = (renta, calcularCredito) => {
-    let metodoPago = 0;
-    while(metodoPago != 3){
-        metodoPago = parseInt(prompt("1= Ver resultados de simulacion; \n2= puedo optar? \n3= Salir"));
-        switch(metodoPago){
-        case 1:
-            console.table(calcularCredito)
-            break
-        case 2:
-            evaluacionComercial(renta, calcularCredito['totalCredito'])
-            break
-        }
+        notificacion.className += " alert-danger";
+        notificacion.hidden = false;
+        notificacion.innerHTML = evComercial.mensaje
     }
 }
 
